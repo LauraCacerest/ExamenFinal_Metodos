093f262b4c01567ae75023fc8b927b887021b294
lauracacerest-e-enfinal_metodos-fhqwmmep <lauracacerest-e-enfinal_metodos-fhqwmmep@local>
2018-05-25 01:01:57 +0000

    ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,10 +1,19 @@
 # Ejercicio 2
 # Complete el siguiente codigo para recorrer la lista `x` e imprima
 # los numeros impares y que pare de imprimir al encontrar un numero mayor a 800
+
 import numpy as np
+n=0
 
 x = np.int_(np.random.random(100)*1000)
-print(x)
+
+while (n>800)
+    if (x%2==0 )
+        print ('Este numero es par')
+    else:
+        
+        
+
 
 
 
