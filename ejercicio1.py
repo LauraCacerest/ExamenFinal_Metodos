859a391820ca8bb8b1841f20b342f6aa7ef4917e
lauracacerest-e-enfinal_metodos-fhqwmmep <lauracacerest-e-enfinal_metodos-fhqwmmep@local>
2018-05-25 01:01:09 +0000

    ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -5,8 +5,17 @@
 
 import numpy as np
 
+
 x = np.linspace(0,2.,10)
 fx = np.array([0., 0.0494, 0.1975, 0.4444, 0.7901,1.2346 , 1.7778, 2.4198, 3.1605, 4.])
+
+def derivada(x,fx)
+    dx=fx
+    d2x=fx
+    
+    
+    
+    
 
 
 
