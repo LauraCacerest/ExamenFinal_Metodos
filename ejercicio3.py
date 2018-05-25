add4081d2e36f23a92643937afb8ada3bd3de6e6
lauracacerest-e-enfinal_metodos-fhqwmmep <lauracacerest-e-enfinal_metodos-fhqwmmep@local>
2018-05-25 01:03:06 +0000

    ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -4,6 +4,31 @@
 # Calcule la covarianza entre `u` y `v` e imprima su valor.
 
 import numpy as np
+import matplotlib
+matplotlib.use('Agg') 
+import matplotlib.pyplot as plt
+
 t = np.array([0.,0.1,0.2,0.3,0.4,0.5,0.6, 0.8, 0.9])
 u = np.array([12.,45.,6.,78.,34.,22.,-10.,31.,-27.])
 v = np.array([3.,11.,1.3,37.,11.,6.,-23.,7.,7.])
+
+plt.plot(u,v)
+plt.savegfig('serie.png')
+plt.show()
+
+def covarianza(u,v)
+
+    valores=[]
+    num_vectores=9
+    
+    promedio_u=sum(u)/len(u)
+    promedio_v=sum(v)/len(v)
+    
+for i in range vectores():
+    
+
+n
+
+
+
+
